1ffffe1b65bf4846155e39f7b500d8381200b8d5
McGiver- <gkolampas@gmail.com>
2016-02-04 21:42:33 -0500

    change background and kosta says i should have it white, we will see

--- bootstrap/js/MySite.js
+++ bootstrap/js/MySite.js
@@ -1,4 +1,19 @@
+<!-- validateEmail -->
 function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
+}
+
+<!-- Change Hackathons background -->
+function changeHackathonsBackground(){
+  document.getElementById("aboutGeorge").style.background = 'url(assets/montrealAtNight.jpg)';
+  document.getElementById("aboutGeorge").style.backgroundSize = "100%"
+  document.getElementById("aboutGeorge").style.backgroundRepeat = 'fixed'
+}
+
+<!-- Change Welcome background -->
+function changeWelcomeBackground(){
+  document.getElementById("aboutGeorge").style.background = 'white';
+  document.getElementById("aboutGeorge").style.backgroundSize = "100%"
+  document.getElementById("aboutGeorge").style.backgroundRepeat = 'fixed'
 }
